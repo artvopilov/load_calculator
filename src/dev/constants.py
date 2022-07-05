0ce3a49fa763599398c83cd8478c57ddc96ca2e1
Artem Vopilov <mister.0025@yandex.ru>
2022-07-05 15:15:05 +0300

    vertical iterator fix

--- src/dev/constants.py
+++ src/dev/constants.py
@@ -2,12 +2,12 @@ from src.parameters.container_parameters import ContainerParameters
 from src.parameters.shipment_parameters import ShipmentParameters
 
 
-AUTO_CONTAINERS = [
-    ContainerParameters('20DV', length=5895, width=2350, height=2393, lifting_capacity=28200),
-    ContainerParameters('40DV', length=12032, width=2350, height=2393, lifting_capacity=28800),
-    ContainerParameters('40HQ', length=12032, width=2350, height=2697, lifting_capacity=28620),
-    ContainerParameters('45HQ', length=13556, width=2350, height=2697, lifting_capacity=27600),
-]
+AUTO_CONTAINERS = {
+    ContainerParameters('20DV', length=5895, width=2350, height=2393, lifting_capacity=28200): -1,
+    # ContainerParameters('40DV', length=12032, width=2350, height=2393, lifting_capacity=28800): -1,
+    # ContainerParameters('40HQ', length=12032, width=2350, height=2697, lifting_capacity=28620): -1,
+    # ContainerParameters('45HQ', length=13556, width=2350, height=2697, lifting_capacity=27600): -1
+}
 CONTAINER_COUNTS = {
     ContainerParameters('Фура', length=5895, width=2350, height=2393, lifting_capacity=28200): 1,
 }
@@ -33,10 +33,8 @@ SHIPMENT_COUNTS_2 = {
                        height_as_height=True, length_as_height=True, width_as_height=True, extension=0.01): 3,
 }
 SHIPMENT_COUNTS_3 = {
-    ShipmentParameters('', '', length=1000, width=500, height=300, weight=100, color='blue', can_stack=True,
+    ShipmentParameters('', '', length=1000, width=1000, height=400, weight=5, color='blue', can_stack=True,
                        height_as_height=True, length_as_height=True, width_as_height=True, extension=0.01): 10,
-    ShipmentParameters('', '', length=600, width=300, height=200, weight=50, color='green', can_stack=True,
-                       height_as_height=True, length_as_height=True, width_as_height=True, extension=0.01): 10,
-    ShipmentParameters('', '', length=500, width=500, height=300, weight=30, color='red', can_stack=False,
-                       height_as_height=True, length_as_height=True, width_as_height=True, extension=0.01): 10,
+    ShipmentParameters('', '', length=100, width=100, height=400, weight=50, color='green', can_stack=True,
+                       height_as_height=True, length_as_height=True, width_as_height=True, extension=0.01): 10
 }

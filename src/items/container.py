82bcf77561c85ab825e898c3de1e6c44dc38b6a1
Artem Vopilov <mister.0025@yandex.ru>
2024-01-09 00:27:17 +0300

    remove extra comments

--- src/items/container.py
+++ src/items/container.py
@@ -117,14 +117,6 @@ class Container(Item[ContainerParameters], VolumeItem, NameItem):
         max_points = self._points_manager.get_closing_points(point)
         for max_point in max_points:
             v = VolumeParameters.from_points(point, max_point)
-            # if max_point == Point(5894, 1203, 2392) or point == Point(2561, 1203, 0):
-            # if point == Point(2561, 1203, 0):
-            #     logger.debug(f'Loading point: {point}')
-            #     logger.debug(f'Available max point: {max_point}')
-            #     logger.debug(f'Available volume: {v}')
-            #     for p in self.min_point_to_id.keys():
-            #         if p.y == 1204:
-            #             logger.debug(f'Loaded points with 1204 y: {p}')
             if v.length < shipment_params.get_loading_length():
                 continue
             if v.width < shipment_params.get_loading_width():
